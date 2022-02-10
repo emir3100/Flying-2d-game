2c7c73c5a3bf558240143b0b20c7eced5af17de7
Emir Jasarevic <emir2000@hotmail.se>
2022-02-10 23:36:34 +0100

    Added animations

--- Flying to Tsuki/Assets/Scripts/Player.cs
+++ Flying to Tsuki/Assets/Scripts/Player.cs
@@ -7,10 +7,11 @@ public class Player : MonoBehaviour
     public float PlayerSpeed;
     private Rigidbody2D rb;
     private Vector2 playerDirection;
-
+    private Animator animator;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
 
@@ -19,6 +20,10 @@ public class Player : MonoBehaviour
         float verticalMove = Input.GetAxisRaw("Vertical");
         float horizontalMove = Input.GetAxisRaw("Horizontal");
         playerDirection = new Vector2(horizontalMove, verticalMove).normalized;
+
+        animator.SetFloat("Horizontal", horizontalMove);
+        animator.SetFloat("Vertical", verticalMove);
+        animator.SetFloat("Speed", playerDirection.sqrMagnitude);
     }
 
     private void FixedUpdate()
